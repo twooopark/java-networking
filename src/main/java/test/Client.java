6bcebc18445bd25827ee139eb43995bdd0f74efa
twooopark <twooo.park@gmail.com>
2018-10-31 17:56:12 +0900

    객체형 프로토콜로 변경

--- src/main/java/test/Client.java
+++ src/main/java/test/Client.java
@@ -18,30 +18,31 @@ public class Client {
             OutputStream os = socket.getOutputStream();
             ObjectOutputStream oos = new ObjectOutputStream(os);
 
+            System.out.println("[client] /명령어 입력");
+            BufferedReader bri = new BufferedReader(new InputStreamReader(System.in));
+            String cmd = bri.readLine();
+
+            String fileName = System.getProperty("user.dir")+"\\src\\main\\java\\test\\Google.html";
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+            StringBuffer sb = new StringBuffer();
             String inStr = null;
+            while( (inStr = br.readLine())!=null ){
+                sb.append(inStr);
+            }
+
             BasicPacket bp = null;
-
-            System.out.println("[client] /명령어 내용 입력");
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            inStr = br.readLine();
-            inStr = "/PRINT Byte Stream부터 알아보자. Byte St 보면 그림도 Byte들로 이루어져 있고, 텍스트도 Byte로 이루어져 있다. 그리고 zip이나 jar같은 압축 파일도 일단은 Byte로 되어 있다. 이 Byte들이 적절하게 변환되면 의미 있는 데이터가 되는 것이다. Byte Stream의 경우에는 원시 Byte를 그대로 주고 받겠다는 의미를 담고있다.";
-
-            String[] data = inStr.split(" ", 2);
-
             //명령어 별 처리
-            String cmd = data[0].toUpperCase();
+            cmd = cmd.toUpperCase();
             if ("/PRINT".equals(cmd)) {
-                bp = new BasicPacket(Command.PRINT, data[1].length(), data[1].getBytes());
+                bp = new BasicPacket(Command.PRINT, sb.toString().length(), sb.toString().getBytes());
             } else if ("/QUIT".equals(cmd)) {
                 bp = new BasicPacket(Command.QUIT);
             } else {
                 System.out.println("[client] command error");
             }
-
-            if (bp != null) {
+            if (bp != null)
                 oos.writeObject(bp);
-            }
 /*
             //CSV 변환
             String csv = String.format("%s,%s,%s", bp.getHeader().getCmd(), bp.getHeader().getSize(), new String(bp.getData()));
